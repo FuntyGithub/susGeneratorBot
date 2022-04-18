a16ec608b2b71001c4402bd68297ac93403ebb97
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2022-04-19 01:36:48 +0200

    v1.0.1
    some logic improvements

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ async function createSusImage(imageURL, size = {x: 128, y: 71}, bgColor = '#0000
         let userImage = await loadImage(imageURL);
         ctx.drawImage(userImage , 0, 0, canvas.width, canvas.height);
 
+        // draw eyes
+        let eyes = await loadImage("./img/eyes.png");
+        ctx.drawImage(eyes , 0, 0, canvas.width, canvas.height);
+
         // set global composite operation so we can draw the eyes transparently
         ctx.globalCompositeOperation = "xor";
 
         // draw temlpate eyes
         let templateImage = await loadImage("./img/template.png");
         ctx.drawImage(templateImage , 0, 0, canvas.width, canvas.height);
-
-        // draw eyes
-        let eyes = await loadImage("./img/eyes.png");
-        ctx.drawImage(eyes , 0, 0, canvas.width, canvas.height);
 
         // return image
         let img = new Discord.MessageAttachment(canvas.toBuffer(), "sus.png");
